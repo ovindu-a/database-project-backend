a34da1aed39b7d630dbd6bd139236738b5aa3b3d
Pasindu Rasanga <pasindurasanga.22@cse.mrt.ac.lk>
2024-08-24 23:13:51 +0530

    Cors issue fixed

--- src/server.js
+++ src/server.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const app = express();
 const dotenv = require('dotenv');
+const cors = require('cors');
+
+app.use(cors());
 
 console.log("Starting server setup...");
 
